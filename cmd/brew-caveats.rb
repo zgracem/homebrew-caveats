9a44c2ef8204798722e63098dc5aa612963da510
Z. Grace Moreau <zgracem@users.noreply.github.com>
2019-10-28 17:12:44 -0600

    streamline syntax

--- cmd/brew-caveats.rb
+++ cmd/brew-caveats.rb
@@ -2,10 +2,6 @@
 
 require "formula"
 
-formulae_with_caveats =
-  Formula.installed.sort.map(&:to_hash).select { |f| f["caveats"].present? }
-
-formulae_with_caveats.each do |formula|
-  ohai formula["name"]
-  puts formula["caveats"]
-end
+Formula.installed.sort.map(&:to_hash)
+       .select { |f| f["caveats"].present? }
+       .each { |f| ohai f["name"], f["caveats"] }
