fdfd0f7c93b1b7c164edf82b1638e51a6a1f900b
Z. Grace Moreau <zgracem@users.noreply.github.com>
2024-10-18 13:12:13 -0600

    extremely minor improvements

--- cmd/caveats.rb
+++ cmd/caveats.rb
@@ -9,8 +9,8 @@ module Homebrew
     class Caveats < AbstractCommand
       cmd_args do
         description <<~HELP
-          Display caveats for an installed <formula>, or for all installed
-          formulae if no argument is provided.
+          Display caveats for an installed <formula> or <cask>,
+          or for all installed formulae and casks if no argument is provided.
         HELP
         switch "--formula", "--formulae",
                description: "Treat all named arguments as formulae."
@@ -19,19 +19,19 @@ module Homebrew
 
         conflicts "--formula", "--cask"
 
-        named_args [:formula, :cask]
+        named_args %i[formula cask]
       end
 
       def run
-        formulae = args.named.to_formulae_and_casks.presence || all_caveats_installed
+        targets = args.named.to_formulae_and_casks.presence || all_caveats_installed
 
-        formulae.each do |f|
-          caveats = f.caveats
+        targets.each do |targets|
+          caveats = targets.caveats
 
           if caveats.present?
-            ohai f.name, caveats.to_s
+            ohai targets.name, caveats.to_s
           else
-            opoo "No caveats for #{f.name}!"
+            opoo "No caveats for #{targets.name}!"
           end
         end
       end
